a88b0fa8d4b4f05a4dd2de55ae0008daecd72a2f
Rajiv karki <rajiv.karki@outlook.com>
2018-04-10 23:10:20 -0400

    All foodtracker files

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Rajiv Karki on 2018-02-25.
+//  Copyright © 2018 RK Home. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        photoImageView.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,20 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Rajiv Karki on 2018-02-25.
+//  Copyright © 2018 RK Home. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+    
+    //MARK: Meal Class Tests
+    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        
+    }
+    
+}
